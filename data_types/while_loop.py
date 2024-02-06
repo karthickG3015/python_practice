18f39f6f3469ae2903d86b46b92d24c31d36fc25
karthickG3015 <karthickG3015@gmail.com>
2024-02-06 15:53:37 +0530

    while_loop

--- data_types/while_loop.py
+++ data_types/while_loop.py
@@ -21,18 +21,93 @@
 #     print("sorry, you failed!")
         
 
-correct = "O.P.S"
-chances = 3
-count = 0
-special_character = ["!","@","#","*","()",")","&"]
-while count < chances:
-    k = input("enter the name = ")
-    if k.isdigit() or k in special_character:
-        print("You enter wrong")
-        break
-    else:
-        count += 1
-        if k == correct:
-            print("You Won!")
-            break
-        
+# correct = "O.P.S"
+# chances = 3
+# count = 0
+# special_character = ["!","@","#","*","()",")","&"]
+# while count < chances:
+#     k = input("enter the name = ")
+#     if k.isdigit() or k in special_character:
+#         print("You enter wrong")
+#         break
+#     else:
+#         count += 1
+#         if k == correct:
+#             print("You Won!")
+#             break
+
+# i = 0
+# while i < 10:
+#     i += 1
+#     print(i, end="#")
+#     print()
+
+# while True:
+#     print("please type your name")
+#     name = input()
+#     if name == "karthi":
+#         break
+# print("thank you, you enter the correct name")
+
+# finding the least common multiple of 4 and 7
+# x = 0
+# while True:
+#     x += 1
+#     if not(x%4 or x%7):
+#         break
+# print(x)
+
+# i = 0
+# while i < 10:
+#     i += 1
+#     if i == 6:
+#         continue
+#     print(i)
+
+# i = 1 
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print("i is not less than 5")
+
+# positive number
+# num = 0 
+# count = 0 
+# sum = 0
+
+# while num >= 0:
+#     num = int(input("enter the positive number: "))
+#     if num >= 0:
+#         count = count + 1
+#         sum = sum + num
+# avg = sum / count
+# print("sum of the number: ", sum, "avarage: ", avg)
+
+# random numbers
+import random
+n = random.randint(1 , 100)
+print(n)
+guess = int(input("enter an integer between 1 to 100: "))
+
+while guess != n:
+    if guess < n:
+        print("your guess is low")
+        guess = int(input("enter an interger between 1 to 100: "))
+    elif guess > n:
+        print("your guess is high")
+        guess = int(input("enter an integer between 1 to 100: "))
+else:
+    print("your guess is correct")
+    
+    
+
+
+
+
+
+
+
+
+
+
