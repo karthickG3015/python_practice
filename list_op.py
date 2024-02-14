85e3509740bdfba0fcda90b68a848c550862a92c
karthickG3015 <karthickG3015@gmail.com>
2024-02-14 12:36:58 +0530

    tuple

--- list_op.py
+++ list_op.py
@@ -97,7 +97,26 @@ print(rep)                  # (or) print(rep*5) there are same
 # del emp                   # delete the tuples
 # print(emp)
 num1 = (1,2,3,8,9,5,3,3,3,4)
-k = num1.count(3)
+k = num1.count(3)           # count the number of variable
 print(k)
-A = sum(num1)
+A = sum(num1)               # sum of the varable
 print(A)
+B = len(num)                # finding lenth of the variable 
+print(B)
+a = min(num1)               # minimum number of the variable
+print(a)
+b = max(num1)               # maximum number of the variable
+print(b)
+c = sorted(num1)             # sorted the tuple variable
+print(c)
+my_list = [3,4,1,4,4,3,9,6,8,5]
+print(my_list)
+print(type(my_list))
+my_tuple = tuple(my_list)           # convert to tuple
+print(my_tuple)
+print(type(my_tuple))
+new_list = [(1,2,3),(4,5,6)]                    # nesting tuple in a list
+new_list.append(("list","tuple","insert"))         
+print(new_list)
+new_list.remove((1,2,3))
+print(new_list)
