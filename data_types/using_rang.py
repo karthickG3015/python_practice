f0c43a7812e82ffabd2599e807e328e3a966709a
karthickG3015 <karthickG3015@gmail.com>
2024-02-28 14:41:56 +0530

    sum

--- data_types/using_rang.py
+++ data_types/using_rang.py
@@ -1,6 +1,16 @@
-previous_no = 0
+# previous_no = 0
 
-for i in range(10):
-    sum = previous_no + i
-    print("current number", i ,"previous number", previous_no ,"The sum is: ", sum)
-    previous_no = i
+# for i in range(10):
+#     sum = previous_no + i
+#     print("current number", i ,"previous number", previous_no ,"The sum is: ", sum)
+#     previous_no = i
+
+
+# # # Print characters from a string that are present at an even index number # # #
+
+str = "pynative"
+
+for i in range(len(str)):
+    if i%2 == 0:
+# for i in range(0,len(str),2):
+        print(str[i])
