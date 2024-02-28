0fe643e96ab88db365a425b743cc49bbfd6b5042
karthickG3015 <karthickG3015@gmail.com>
2024-02-28 15:41:51 +0530

    pro

--- data_types/ProductOfTwo.py
+++ data_types/ProductOfTwo.py
@@ -1,10 +1,16 @@
-def ProductOfTwo (num1 , num2):
-    product = num1 * num2
+# def ProductOfTwo (num1 , num2):
+num1 = int(input())
+num2 = int(input())
 
-    if product <= 1000:
-        return product
-    else:
-        return num1 + num2
+product = num1 * num2
+sum = num1 + num2
+
+if product <= 1000:
+        # return product
+    print(product)
+else:
+        # return num1 + num2
     
-result = ProductOfTwo(30,30)
-print(result)
+    
+# result = ProductOfTwo(30,30)
+    print(sum)
