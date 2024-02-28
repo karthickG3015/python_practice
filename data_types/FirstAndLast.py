696942be047bc4462d82f06bf782a6c85336b88a
karthickG3015 <karthickG3015@gmail.com>
2024-02-28 19:16:15 +0530

    co

--- data_types/FirstAndLast.py
+++ data_types/FirstAndLast.py
@@ -1,3 +1,4 @@
+### Write a function to return True if the first and last number of a given list is same. If numbers are different then return False###
 def first_and_last(numberlist):
     print("the list is: ",numberlist)
 
