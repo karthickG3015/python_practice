cfa8abc829084103e25f5e582159cf59ddfb02ab
karthickG3015 <karthickG3015@gmail.com>
2024-02-29 10:57:42 +0530

    div

---
+++ data_types/new_exe.py/number_div.py
@@ -0,0 +1,5 @@
+### Iterate the given list of numbers and print only those numbers which are divisible by 5 ###
+new_list = [10, 20, 33, 46, 55]
+for i in new_list:
+    if i % 5 == 0:
+        print(i)
