0bad142664aed69595c7d7e876978a4a92c41c70
karthickG3015 <karthickG3015@gmail.com>
2024-03-05 19:18:32 +0530

    pattern

--- data_types/new_exe.py/table.py
+++ data_types/new_exe.py/table.py
@@ -1,4 +1,9 @@
-for i in range(1,11):
-    for j in range(1,11):
-        print(i*j, end=" ")
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(i*j, end=" ")
+#     print("")
+
+for i in range(6,0,-1):
+    for j in range(0, i-1):
+        print("*",end=" ")
     print("")
